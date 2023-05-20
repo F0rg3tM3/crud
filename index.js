f6f36f94c383319704e475699b8a3f0925a143d8
F0rg3tM3 <Cooler_993@yahoo.com>
2023-05-21 02:25:45 +0800

    Test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const database = "";
+
+function CRUD_Create() {
+	console.log("Create Function");
+}
+
+function CRUD_Read() {
+	console.log("Read Function");
+}
+
+function CRUD_Update() {
+	console.log("Update Function");
+}
+
+function CRUD_Delete() {
+	console.log("Delete Function");
+}
