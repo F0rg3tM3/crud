0665c117e47b729a03e939d745e653f11ce0c919
F0rg3tM3 <Cooler_993@yahoo.com>
2023-05-23 17:26:03 +0800

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const firebaseConfig {
-	apiKey: 		"",
-	authDomain: 		"",
-	projectId: 		"",
-	storageBucket:		"",
-	messagingSenderId:	"",
-	appId:			"",
-	measurementId:		"",
+	apiKey: 		"AIzaSyBu65k6Ik4735F9foAilaFsLgATPVBZ8go",
+	authDomain: 		"crud-cc73f.firebaseapp.com",
+	projectId: 		"crud-cc73f",
+	storageBucket:		"crud-cc73f.appspot.com",
+	messagingSenderId:	"934235522113",
+	appId:			"1:934235522113:web:d92fb427dfcabaf87c72c0",
+	measurementId:		"G-LQ24E9LHTF",
 	databaseURL:		"https://crud-cc73f.firebaseio.com"
 }
 
